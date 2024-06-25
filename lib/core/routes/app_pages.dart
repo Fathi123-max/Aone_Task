4c487f0992ec50af4403b412e9c386d99c533c1a
Fathi wehba <Fathiwehba5@gmail.com>
2024-06-25 20:20:34 +0400

    clean up the Project Before Start

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -1,8 +1,5 @@
 import 'package:get/get.dart';
 
-import '../../modules/home/bindings/home_binding.dart';
-import '../../modules/home/views/home_view.dart';
-
 part 'app_routes.dart';
 
 class AppPages {
@@ -10,11 +7,5 @@ class AppPages {
 
   static const INITIAL = Routes.HOME;
 
-  static final routes = [
-    GetPage(
-      name: _Paths.HOME,
-      page: () => const HomeView(),
-      binding: HomeBinding(),
-    ),
-  ];
+  static final List<GetPage<dynamic>> routes = [];
 }
